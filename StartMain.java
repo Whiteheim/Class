75dd5eb7b881ec57d0e05c4870d2dc2a52c8116f
Whiteheim <110002385+Whiteheim@users.noreply.github.com>
2022-07-28 18:04:10 +0900

    Add files via upload

---
+++ StartMain.java
@@ -0,0 +1,127 @@
+
+public class StartMain {
+	// 주석(Comment) : 필기, 기능설명 용도 
+	// / : Slash
+	// \ : BackSlash 
+	
+	// 한 줄 주석
+	
+	/*  /* <<주석 여러개 가능 (코드내에서 볼 수 있음)
+	 * 가능
+	 * 가능
+	 */
+	/**
+	 *  /** <<다른사람 들에게 보여주기 위한 설명서 (HTML 문서로 남음)
+	 *  그런데 여러 주석을 더한
+	 *  @author beaver
+	 *  @version 1.8.어쩌고
+	 *  
+	 */
+	
+	// Java!
+	/* 
+	 * 세심하게 하나하나 개발자가 손을 봐 줘야 함.(귀찮)
+	 * 상대적으로 저급언어에 속함.
+	 * => 먼저 Java를 공부 하고 나중에 다른 언어를 습득할 때 좀 더 쉽게 습득 할 수 있다.
+	 * 전 세계적으로 많이 사용하는 언어 중 하나
+	 * => 개발자들이 작업한 자료들을 공개 / 공유하는 문화 존재
+	 * Oracle JDK vs Open JDK
+	 * Sun micro systems에서 만든 제품 - Java
+	 * 이후 Oracle이 Sun을 인수 -> Java = Oracle 소유
+	 * 		Oracle JDK(OpenJDK + 추가기능) : 유료
+	 * 		Open JDK : 무료
+	 */
+
+	// 많은 회사들이 OpenJDK 사용중.
+	
+	// LTS (Long Term Support) : 장기간 지원
+	
+	// Open JDK
+	/* .msi / .zip
+	 * .msi /설치 버전 : 하나의 컴퓨터에서 Eclips를 여러개 사용 가능. 
+	 * .zip /압축 버전 : 하나의 컴퓨터에서 Eclips를 하나만 사용 가능.
+	 * 	
+	 * Java를 사용하기 위해 JDK(Java Development Kit)가 설치가 되어있어야 함.
+	 * JRE + 실행 키트 를 합친 개념.	
+	 * JRE (Java Runtime Environment): Java로 만든 프로그램을 실행할 수 있게 함.
+	 */
+
+	//javac.exe : JDK에 포함이 되어있는 주요한 Java의 컴파일러(Compiler)
+	/*
+	 * Hello 텍스트 파일을 ByteCode(실행을 하기 위한 2진 표현법)로 변환
+	 * 	=>이것을 Compile이라고 함.
+	 * 컴파일 과정을 해주는 프로그램 javac.exe (인간언어(.txt) -> 번역 -> 기계어(.class))
+	 * .txt 파일을 javac로 실행하면, .class파일이 생성 됨	 
+	 */
+		
+	
+	//java.exe : 컴파일 된 .class파일을 실행시켜주는 프로그램
+	/* 기계어 파일(.class) -> 실행
+	 */
+	// Eclips
+	/* 통합 개발 환경 중 하나
+	 * intelliJ, VSCode, ... etc.
+	 * Eclips = 무료
+	 */
+	 
+	// Developer vs Enterprise
+	// 기본 서비스 제공 vs 기본 서비스 + 추가 기능
+	
+	// Encoding
+	/* 인간이 입력한 글자를 컴퓨터가 사용 가능하도록 신호로 변환하는 것.
+	 * EUC-KR, MS949, UTF-8, ... etc.
+	 * 
+	 */
+
+	// 알아두면 유용한 단축키
+	/* Ctrl + s = 저장(save)
+	 * Ctrl + a = 전체선택(all)
+	 * Ctrl + m = 화면 비율 조정
+	 * Ctrl + Shift + / : 여러 줄 주석 처리 (문장 스크롤 후 사용)
+	 * Ctrl + alt + 방향키(↑↓) : 줄 복사 (문장 스크롤 후 사용, 방향키 방향으로 복사)
+	 * Ctrl + Shift + +/-(텐 키) : 글자 크기 조절
+	 * Ctrl + d : 삭제 (Delete)
+	 * Ctrl + Space bar : 자동완성 
+	 * Ctrl + Shift + f : 줄 정리 기능
+	 * Ctrl + F11 : 실행 기능
+	 * Alt + 방향키(↑↓) : 줄 이동 (문장 스크롤 후 사용, 방향키 방향으로 이동) 
+	 */
+	
+	
+	
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
