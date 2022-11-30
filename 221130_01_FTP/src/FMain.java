b3c12b3ab3f41b7552780224addadbb619c91cde
Whiteheim <110002385+Whiteheim@users.noreply.github.com>
2022-11-30 18:09:56 +0900

    Add files via upload

---
+++ 221130_01_FTP/src/FMain.java
@@ -0,0 +1,37 @@
+
+public class FMain {
+	public static void main(String[] args) {
+		System.out.println("Long Time no see");
+		System.out.println("다까먹음");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
